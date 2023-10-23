8bdb248490d782630ab69b4667a2c08ca2422404
cekator3 <hamsterdreams@inbox.ru>
2023-10-23 22:57:20 +0500

    Added descriptions for stubs of math expression generator subsystem.

--- app/Model/MathExpressionGenerator.js
+++ app/Model/MathExpressionGenerator.js
@@ -1,14 +1,31 @@
-///////////////////////////////////////////////
-//A subsystem for generating math expressions.
+////////////////////////////////////////////////////////////////////
+//A subsystem for generating math expressions from given components.
 //
 //It will be used, for example, if the user enters
 //math expression using buttons rather than direct input
-///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
-function getMathExpression() { }
+/**
+ * Generates math expression from provided components.
+ * @returns {string}
+ */
+export function getMathExpression() {}
 
-function clearMathExpression() {}
+/**
+ * Deletes all contents of math expression.
+ * @returns {void}
+ */
+export function clearMathExpression() {}
 
-function removeLastComponentFromMathExpression() {}
+/**
+ * Removes last component from math expression.
+ * @returns {void}
+ */
+export function removeLastComponentFromMathExpression() {}
 
-function addComponentToMathExpression() {}
+/**
+ * Adds a component to a math expression.
+ * @param {string} component - component of math expression, such as number, operator, etc.
+ * @returns {void}
+ */
+export function addComponentToMathExpression(component) {}
